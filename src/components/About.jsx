9c3d7881747ef4d8241796123b6392c3c92cb102
bhanu1999 <96969009+bhanu1999@users.noreply.github.com>
2023-01-09 10:41:12 -0800

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
                 <div className='pb-8'>
                     <p className='text-4xl font-bold  inline border-b-4 border-gray-500 '>About</p>
                 </div>
-                <p className='text-xl mt-20'>Hey, I'm Bhanu Prakash Sakinala, a computer science graduate student at university of cincinnati. I’m an full stack developer enthusiast, with interests in data structures, algorithms and machine learning. Problem solving is something I thrive on and have a thirst for new technologies and programming languages </p>
+                <p className='text-xl mt-20'>Hey, I'm Bhanu Prakash Sakinala, a computer science graduate student at University of Cincinnati. I’m an full stack developer enthusiast, with interests in data structures, algorithms and machine learning. Problem solving is something I thrive on and have a thirst for new technologies and programming languages </p>
                 <br />
                 <p className='text-xl'>I have 1 years of professional working experience as a full stack developer where i mostly worked on tech stacks like Angular, Typescript, Spring, Java </p>
             </div>
